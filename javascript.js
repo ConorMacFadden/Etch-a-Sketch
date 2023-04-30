9f9bb2e0bb6872f2d22eb8e7b38ac4df498c8aca
ConorMacFadden <90274527+ConorMacFadden@users.noreply.github.com>
2023-04-30 18:55:24 +0100

    Add resize and reset buttons with functions

--- javascript.js
+++ javascript.js
@@ -1,28 +1,51 @@
-let gridSize = 10;
+let gridSize = 16;
 let gridSquareWidth = 50;
 let gridSquareBorderWidth = 1;
 let gridContainerWidth = gridSquareWidth*gridSize;
 
 // Build the grid
-const gridContainer = document.querySelector('.gridContainer');
-for(let i=0;i<gridSize;i++){ // create rows for the grid
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('gridRow'); // add the gridRow class so we can apply css
+function buildGrid(size){
 
-    //create the grid squares in the row
-    for(let j=0;j<gridSize;j++){
-        const boxDiv = document.createElement('div');
-        boxDiv.classList.add('gridSquare');
-        boxDiv.style.flex = "1";
-        //boxDiv.style.minHeight = `${gridSquareWidth-(2*gridSquareBorderWidth)}px`;
+    const gridContainer = document.querySelector('.gridContainer');
 
-        rowDiv.appendChild(boxDiv);
+    while(gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild)
     }
-    gridContainer.appendChild(rowDiv);
-}
 
+    for(let i=0;i<size;i++){ // create rows for the grid
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('gridRow'); // add the gridRow class so we can apply css
+
+        //create the grid squares in the row
+        for(let j=0;j<size;j++){
+            const boxDiv = document.createElement('div');
+            boxDiv.classList.add('gridSquare');
+            boxDiv.style.flex = "1";
+
+            rowDiv.appendChild(boxDiv);
+        }
+        gridContainer.appendChild(rowDiv);
+    }
+}
 document.addEventListener("mouseover", (e) => {
     if(e.target.classList.contains("gridSquare")){
         e.target.style.backgroundColor = "black";
     } 
 });
+
+// functions all declared, now run the following to complete page setup
+buildGrid(gridSize);
+
+const btnResize = document.querySelector('.setGridSize');
+btnResize.addEventListener('click', (e) => {
+    let size = Math.floor(prompt("Enter the required grid size","16"));
+    if (size != NaN) {
+        if(size > 100) size = 100;
+        if (size == gridSize) return;
+        gridSize = size;
+        buildGrid(size);
+   }
+});
+
+const btnReset = document.querySelector('.reset');
+btnReset.onclick = function(){buildGrid(gridSize)}
