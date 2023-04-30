640b8f9ffc0471db66d57020bea75f5da2406872
ConorMacFadden <90274527+ConorMacFadden@users.noreply.github.com>
2023-04-30 17:29:34 +0100

    Make the grid size dynamic
    The grid size is no longer fixed, the grid fills the available space.
    This was done by creating flex containers for each row, setting the
    flex direction of the parent container to column and setting the
    aspect-ratio css property to 1/1 so the container remains square.

--- javascript.js
+++ javascript.js
@@ -1,28 +1,26 @@
-let gridSize = 30;
+let gridSize = 10;
 let gridSquareWidth = 50;
 let gridSquareBorderWidth = 1;
 let gridContainerWidth = gridSquareWidth*gridSize;
 
-
+// Build the grid
 const gridContainer = document.querySelector('.gridContainer');
-//gridContainer.style.width = `${gridContainerWidth}px`;
-for(let i=0;i<gridSize;i++){
+for(let i=0;i<gridSize;i++){ // create rows for the grid
     const rowDiv = document.createElement('div');
-    rowDiv.classList.add('gridRow');
+    rowDiv.classList.add('gridRow'); // add the gridRow class so we can apply css
+
     for(let j=0;j<gridSize;j++){
         const boxDiv = document.createElement('div');
         boxDiv.classList.add('gridSquare');
-        boxDiv.style.minWidth = `${gridSquareWidth-(2*gridSquareBorderWidth)}px`;
+        boxDiv.style.flex = "1";
         boxDiv.style.minHeight = `${gridSquareWidth-(2*gridSquareBorderWidth)}px`;
         rowDiv.appendChild(boxDiv);
     }
-    gridContainer.style.display = "flex";
     gridContainer.appendChild(rowDiv);
 }
 
 document.addEventListener("mouseover", (e) => {
     if(e.target.classList.contains("gridSquare")){
         e.target.style.backgroundColor = "black";
-    }
-    
+    } 
 });
