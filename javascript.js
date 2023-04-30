7af4196e643ec6cb0dd79cef6b3ebb8bfe140196
ConorMacFadden <90274527+ConorMacFadden@users.noreply.github.com>
2023-04-30 18:03:19 +0100

    Fixes grid resizing issue with fixed height
    The javascript had a fixed minimum value for the box height. This caused
    problems with the aspect ratio of the grid container. Some of the flex
    properties were also set on the grid row and grid container flex boxes
    to ensure correct scaling beahviour.
    
    The grid container is now inside a main container and there's a header
    above it.

--- javascript.js
+++ javascript.js
@@ -9,11 +9,13 @@ for(let i=0;i<gridSize;i++){ // create rows for the grid
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('gridRow'); // add the gridRow class so we can apply css
 
+    //create the grid squares in the row
     for(let j=0;j<gridSize;j++){
         const boxDiv = document.createElement('div');
         boxDiv.classList.add('gridSquare');
         boxDiv.style.flex = "1";
-        boxDiv.style.minHeight = `${gridSquareWidth-(2*gridSquareBorderWidth)}px`;
+        //boxDiv.style.minHeight = `${gridSquareWidth-(2*gridSquareBorderWidth)}px`;
+
         rowDiv.appendChild(boxDiv);
     }
     gridContainer.appendChild(rowDiv);
